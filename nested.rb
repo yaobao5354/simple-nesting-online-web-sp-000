a0eb619b02839ebde95f114d15393dc617f8e206
yaoyu bao <yaobao5354@gmail.com>
2019-08-26 22:56:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,8 +105,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     # alans_new_info = "GUI"
-    programmer_hash[:alan_kay][:known_for] = "GUI"
+     alans_new_info = "GUI"
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash
      
 end
 
