9d82f0de12384bfc655674e5e4e1f0f2eb8d4abb
yaoyu bao <yaobao5354@gmail.com>
2019-08-26 22:59:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,6 +127,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:alan_key][:languages] << "Assembly"
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
   programmer_hash
 end
